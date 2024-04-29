227c01957546eca5fe63496a81a8cea86bd67ac0
Ethan <ezlaviol@my.loyno.edu>
2024-04-29 18:48:22 -0500

    Q1 and Q2

---
+++ Q2_2_sem.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <thread>
+#include <semaphore.h>
+using namespace std;
+
+sem_t sem;
+
+void runMeFirst() {
+     cout << "Run me first" << endl;
+}
+
+void runMeSecond() {
+     cout << "I run second" << endl;
+}
+
+void runMeThird() {
+     cout << "I run third" << endl;
+}
+
+void thread_func(int id) {
+    cout << "Thread " << id << " is waiting..." << endl;
+    sem_wait(&sem); // blocking call
+    cout << "Thread " << id << " has acquired the semaphore";
+    cout << endl;
+    if (id == 2) runMeFirst();
+    if (id == 0) runMeSecond();
+    if (id == 1) runMeThird();
+    this_thread::sleep_for(chrono::seconds(2));
+    cout << "Thread " << id << " is releasing the semaphore";
+    cout << endl;
+    sem_post(&sem);
+}
+
+int main() {
+    const int num_threads = 2;
+    // initialize the semaphore with the number of threads
+    sem_init(&sem, 0, num_threads); // 0: thread, 1: process
+    thread threads[num_threads];
+    for (int i = 0; i < num_threads; ++i) {
+        threads[i] = thread(thread_func, i);
+    }
+    for (int i = 0; i < num_threads; ++i) {
+        threads[i].join();
+    }
+    sem_destroy(&sem); // destroy the semaphore
+    return 0;
+}
---
+++ Q3.cpp
@@ -0,0 +1,16 @@
+string webpages[10] = {"Google.com", "Google.com", }
+
+Listen() {
+         request = k) // (0-9)
+         }
+         
+doRequest() {
+            }
+            
+struct {
+       request_id = counter
+       ip = ""
+       website: webpages[k]
+       }
+       
+// push in queue
